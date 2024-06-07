7079e66577d9c077bc2b96b59249372127210413
preethibilla <42039081+preethibilla@users.noreply.github.com>
2024-06-06 18:54:55 -0700

    Time: 36 ms (63.90%) | Memory: 16.5 MB (92.60%) - LeetSync

--- 397-integer-replacement/integer-replacement.py
+++ 397-integer-replacement/integer-replacement.py
@@ -1,12 +1,29 @@
 class Solution:
     def integerReplacement(self, n: int) -> int:
+        # if(n==1):
+        #     return 0
+        
+        # if(n%2==0):
+        #     return 1 + self.integerReplacement(n//2)
+        # else:
+        #     return min(1 + self.integerReplacement(n+1),1 + self.integerReplacement(n-1))
+        visited = dict()
+        return self.getIntegerReplacement(visited, n)
+    def getIntegerReplacement(self, visited, n):
         if(n==1):
             return 0
         
+        if(n in visited):
+            return visited[n]
+        
         if(n%2==0):
-            return 1 + self.integerReplacement(n//2)
+            result = 1 + self.getIntegerReplacement(visited, n//2)
         else:
-            return min(1 + self.integerReplacement(n+1),1 + self.integerReplacement(n-1))
+            result = min(1 + self.getIntegerReplacement(visited, n+1),1 + self.getIntegerReplacement(visited, n-1))
+        visited[n] = result
+        return result
+        
+
         
             
         
