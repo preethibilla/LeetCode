45497251f7c597d3f125fa21160896ed55e27e74
preethibilla <42039081+preethibilla@users.noreply.github.com>
2024-06-09 23:39:37 -0700

    Time: 42 ms (43.71%) | Memory: 18.5 MB (93.41%) - LeetSync

---
+++ 328-odd-even-linked-list/odd-even-linked-list.py
@@ -0,0 +1,19 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def oddEvenList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        if head is None or head.next is None or head.next.next is None:
+            return head
+        odd = head
+        even = even_head = head.next
+        while odd.next and even.next:
+            odd.next = odd.next.next
+            even.next = even.next.next
+            odd = odd.next
+            even = even.next
+        odd.next = even_head
+        return head
+        
