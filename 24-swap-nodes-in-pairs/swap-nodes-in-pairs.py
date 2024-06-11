16d6bf9b3623571c84198eb6ec2976c14f3398d7
preethibilla <42039081+preethibilla@users.noreply.github.com>
2024-06-11 15:35:17 -0700

    Time: 37 ms (44.81%) | Memory: 16.6 MB (31.95%) - LeetSync

--- 24-swap-nodes-in-pairs/swap-nodes-in-pairs.py
+++ 24-swap-nodes-in-pairs/swap-nodes-in-pairs.py
@@ -5,16 +5,24 @@
 #         self.next = next
 class Solution:
     def swapPairs(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        dummy = ListNode(0,head)
-        prev = dummy
-        current = head
-        while current and current.next:
-            nextPair = current.next.next
-            second = current.next
-            second.next = current
-            current.next = nextPair
+        temphead = ListNode(0,head)
+        prev = temphead
+        curr = head
+        while curr and curr.next:
+        #saving the next pair
+            nextSwapPair = curr.next.next
+        # node to be swapped with the current node
+            second = curr.next
+        #swap nodes
+        #second node points to the current node
+            second.next = curr
+        #code node is pointing to the nextPair
+            curr.next = nextSwapPair
+        #prev node is pointing to the second
             prev.next = second
-            prev = current
-            current = nextPair
-        return dummy.next
+        #move the position of the pointers
+            prev =  curr
+            curr = nextSwapPair
+        #return the new head of the list which is the next node of the temphead
+        return temphead.next
         
