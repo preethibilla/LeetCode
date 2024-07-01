1fe613579d6fa6db97f072d0c40c6bb1d9423e79
preethibilla <42039081+preethibilla@users.noreply.github.com>
2024-06-30 17:45:03 -0700

    Time: 39 ms (44.01%) | Memory: 16.5 MB (94.01%) - LeetSync

--- 78-subsets/subsets.py
+++ 78-subsets/subsets.py
@@ -3,11 +3,12 @@ class Solution:
         result = []
 
         def backtrack(start,path):
+            # Append the current subset (path) to the result list
             result.append(path)
+            # Iterate over the remaining elements starting from 'start'
             for i in range(start,len(nums)):
+            # Include nums[i] in the subset and move to the next element
                 backtrack(i+1,path+[nums[i]])
+        # Start the backtracking with an empty subset
         backtrack(0,[])
-
         return result
-
-        
