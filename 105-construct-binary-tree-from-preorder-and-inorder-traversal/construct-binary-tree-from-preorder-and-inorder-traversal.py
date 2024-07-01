77db25817d829b64cc399baf003e321e267bb435
preethibilla <42039081+preethibilla@users.noreply.github.com>
2024-07-01 16:52:05 -0700

    Time: 136 ms (31.73%) | Memory: 88.1 MB (32.06%) - LeetSync

--- 105-construct-binary-tree-from-preorder-and-inorder-traversal/construct-binary-tree-from-preorder-and-inorder-traversal.py
+++ 105-construct-binary-tree-from-preorder-and-inorder-traversal/construct-binary-tree-from-preorder-and-inorder-traversal.py
@@ -9,10 +9,11 @@ class Solution:
         if not preorder or not inorder:
             return None
         
-        rootVal = preorder.pop(0)
+        rootVal = preorder[0]
         root = TreeNode(rootVal)
         mid = inorder.index(rootVal)
 
-        root.left = self.buildTree(preorder,inorder[:mid])
-        root.right = self.buildTree(preorder,inorder[mid+1:])
+        root.left = self.buildTree(preorder[1:mid+1],inorder[:mid])
+        root.right = self.buildTree(preorder[mid+1:],inorder[mid+1:])
         return root
+ 
