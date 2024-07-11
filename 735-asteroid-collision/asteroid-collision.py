b455ba59c7697ce64ec26ded48e7f88351799692
preethibilla <42039081+preethibilla@users.noreply.github.com>
2024-07-10 18:22:48 -0700

    Time: 80 ms (69.28%) | Memory: 17.9 MB (37.61%) - LeetSync

--- 735-asteroid-collision/asteroid-collision.py
+++ 735-asteroid-collision/asteroid-collision.py
@@ -1,33 +1,18 @@
 class Solution:
     def asteroidCollision(self, asteroids: List[int]) -> List[int]:
-        # Initialize an empty stack to keep track of asteroids still moving
         stack = []
-        
-        # Iterate through each asteroid in the input list
         for asteroid in asteroids:
-            # Determine if the current asteroid needs to be pushed to the stack
-            push = True
-            
-            # Handle collisions for asteroids moving to the left
-            while stack and asteroid < 0 < stack[-1]:
-                # Compare sizes of the colliding asteroids
-                if stack[-1] < -asteroid:
-                    # The right-moving asteroid is smaller and explodes
+            while stack and stack[-1] > 0 and asteroid < 0:
+		# determine which asteroids are exploding
+                if abs(stack[-1]) < abs(asteroid):
                     stack.pop()
-                elif stack[-1] == -asteroid:
-                    # Both asteroids are the same size and explode
+		    # considered asteroid might still destroy others so continue checking
+                    continue
+                elif abs(stack[-1]) == abs(asteroid):
                     stack.pop()
-                    push = False
-                    break
-                else:
-                    # The right-moving asteroid is larger, the left-moving asteroid explodes
-                    push = False
-                    break
-            
-            # If the current asteroid is not destroyed, push it to the stack
-            if push:
+                break
+	    # if nothing on the stack, 
+            else:
                 stack.append(asteroid)
-        
-        # Return the state of the asteroids after all collisions
         return stack
         
