938d632b77d549471933ff97e1894834163f9e21
preethibilla <42039081+preethibilla@users.noreply.github.com>
2024-08-01 15:19:22 -0700

    Time: 44 ms (61.12%) | Memory: 16.7 MB (24.72%) - LeetSync

--- 797-rabbits-in-forest/rabbits-in-forest.py
+++ 797-rabbits-in-forest/rabbits-in-forest.py
@@ -1,25 +1,20 @@
 class Solution:
     def numRabbits(self, answers: List[int]) -> int:
-        freq = {}
-        count = 0
+        counts = defaultdict(int)
+        sum_rabbits = 0
 
         for answer in answers:
             if answer == 0:
-                # This must be the only rabbit of its color.
-                count += 1
-            elif answer not in freq or freq[answer] == 0:
-                # This is the first time the color appears.
-                freq[answer] = 1
-                # Add all rabbits having this new color.
-                count += answer + 1
+                sum_rabbits += 1
+            elif counts[answer] == 0:
+                counts[answer] = 1
+                sum_rabbits += answer + 1
+            elif counts[answer] < answer + 1:
+                counts[answer] += 1
             else:
-                # Add one to how many times answer occurred.
-                freq[answer] += 1
-                if freq[answer] > answer:
-                    # If n+1 rabbits have said n,
-                    # this color group is complete.
-                    freq[answer] = 0
-        
-        return count
+                counts[answer] = 1
+                sum_rabbits += answer + 1
+
+        return sum_rabbits
 
         
