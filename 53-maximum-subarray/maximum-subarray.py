9f855a1e61b16b09da41099c3e3ca4e0379d0af2
preethibilla <42039081+preethibilla@users.noreply.github.com>
2024-08-06 15:40:08 -0700

    Time: 546 ms (32.03%) | Memory: 24.6 MB (39.12%) - LeetSync

--- 53-maximum-subarray/maximum-subarray.py
+++ 53-maximum-subarray/maximum-subarray.py
@@ -1,28 +1,21 @@
-class Solution:
-    def maxSubArray(self, nums: List[int]) -> int:
-        # Initialize maxSub to the first element of the array.
-        # This will store the maximum subarray sum found so far.
+class Solution(object):
+    def maxSubArray(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+          #intialise the maxSub to the first element of the array.
         maxSub = nums[0]
-        
-        # Initialize curSum to 0.
-        # This will store the current subarray sum.
+        #curSum to 0
         curSum = 0
-        
-        # Iterate through each element in the array.
+        #iterate through each element in the array.
         for n in nums:
-            # If curSum is less than 0, reset it to 0.
-            # This is because a negative sum will reduce the total sum of any subarray it is part of.
+            #if the curSum is less tham 0 , then we reset it to 0(since negative numbers reduce the sum found so far)
             if curSum < 0:
                 curSum = 0
-            
-            # Add the current element to curSum.
+            #we add the current element to curSum.
             curSum += n
-            
-            # Update maxSub to be the maximum of maxSub and curSum.
-            # This ensures maxSub always holds the highest subarray sum found so far.
-            maxSub = max(curSum, maxSub)
-        
-        # Return the maximum subarray sum.
+            #update the maxSub to be the maximum of maxSub and curSub
+            maxSub = max(curSum,maxSub)
         return maxSub
-
         
