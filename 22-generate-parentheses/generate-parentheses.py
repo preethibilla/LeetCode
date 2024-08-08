708b45ca35106ad1135c370490f720e08edc7a81
preethibilla <42039081+preethibilla@users.noreply.github.com>
2024-08-08 15:18:24 -0700

    Time: 35 ms (75.57%) | Memory: 16.8 MB (38.99%) - LeetSync

--- 22-generate-parentheses/generate-parentheses.py
+++ 22-generate-parentheses/generate-parentheses.py
@@ -1,20 +1,16 @@
 class Solution:
     def generateParenthesis(self, n: int) -> List[str]:
-        def backtrack(curr, open_count, close_count):
-            # If the current string has reached the maximum length
-            if len(curr) == 2 * n:
-                result.append(curr)
+        def backtrack(current,openbrace_count,closebrace_count):
+            nonlocal result
+            if len(current) == n*2:
+                result.append(current)
                 return
-            
-            # If we can still add an opening bracket, do so
-            if open_count < n:
-                backtrack(curr + '(', open_count + 1, close_count)
-            
-            # If we can add a closing bracket (i.e., close a previously opened bracket), do so
-            if close_count < open_count:
-                backtrack(curr + ')', open_count, close_count + 1)
-        
-        result = []
-        backtrack('', 0, 0)
+            if openbrace_count < n:
+                backtrack(current + "(", openbrace_count + 1, closebrace_count)
+            if closebrace_count < openbrace_count:
+                backtrack(current + ")", openbrace_count, closebrace_count+1)
+            return
+        result=[]
+        backtrack("", 0, 0)
         return result
             
