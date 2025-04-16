674c20adb3ef0a848a804bb68fe9a9deba217ac9
preethibilla <42039081+preethibilla@users.noreply.github.com>
2025-04-15 18:47:47 -0700

    Time: 59 ms (67.37%) | Memory: 22.1 MB (17.70%) - LeetSync

--- 53-maximum-subarray/maximum-subarray.py
+++ 53-maximum-subarray/maximum-subarray.py
@@ -1,11 +1,21 @@
-class Solution:
-    def maxSubArray(self, nums: List[int]) -> int:
-        maxSub = float('-inf')
-        currSum = 0
+class Solution(object):
+    def maxSubArray(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+          #intialise the maxSub to the first element of the array.
+        maxSub = nums[0]
+        #curSum to 0
+        curSum = 0
+        #iterate through each element in the array.
         for n in nums:
-            if currSum < 0:
-                currSum = 0
-            currSum += n
-            maxSub = max(maxSub,currSum)
+            #if the curSum is less tham 0 , then we reset it to 0(since negative numbers reduce the sum found so far)
+            if curSum < 0:
+                curSum = 0
+            #we add the current element to curSum.
+            curSum += n
+            #update the maxSub to be the maximum of maxSub and curSub
+            maxSub = max(curSum,maxSub)
         return maxSub
         
