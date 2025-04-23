6f974fa9607ecab49df1021361c5e3c1e24b8b78
preethibilla <42039081+preethibilla@users.noreply.github.com>
2025-04-23 14:36:45 -0700

    Time: 0 ms (100.00%) | Memory: 17.9 MB (92.55%) - LeetSync

--- 22-generate-parentheses/generate-parentheses.py
+++ 22-generate-parentheses/generate-parentheses.py
@@ -1,16 +1,22 @@
 class Solution:
     def generateParenthesis(self, n: int) -> List[str]:
-        def backtrack(current,openbrace_count,closebrace_count):
-            nonlocal result
-            if len(current) == n*2:
-                result.append(current)
+        stack = []
+        res = []
+
+        def backtrack(openN, closedN):
+            if openN == closedN == n:
+                res.append("".join(stack))
                 return
-            if openbrace_count < n:
-                backtrack(current + "(", openbrace_count + 1, closebrace_count)
-            if closebrace_count < openbrace_count:
-                backtrack(current + ")", openbrace_count, closebrace_count+1)
-            return
-        result=[]
-        backtrack("", 0, 0)
-        return result
+
+            if openN < n:
+                stack.append("(")
+                backtrack(openN + 1, closedN)
+                stack.pop()
+            if closedN < openN:
+                stack.append(")")
+                backtrack(openN, closedN + 1)
+                stack.pop()
+
+        backtrack(0, 0)
+        return res
             
