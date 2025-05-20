141183121b2e4e89c76b691b3833e05bd5dc7716
preethibilla <42039081+preethibilla@users.noreply.github.com>
2025-05-19 21:17:34 -0700

    Time: 111 ms (67.09%) | Memory: 37.3 MB (45.56%) - LeetSync

--- 2552-maximum-sum-of-distinct-subarrays-with-length-k/maximum-sum-of-distinct-subarrays-with-length-k.py
+++ 2552-maximum-sum-of-distinct-subarrays-with-length-k/maximum-sum-of-distinct-subarrays-with-length-k.py
@@ -1,20 +1,21 @@
 class Solution:
     def maximumSubarraySum(self, nums: List[int], k: int) -> int:
-        count = defaultdict(int)
-        maxSub = 0
-        currSum = 0
-        l = 0
-        for r in range(len(nums)):
-            currSum += nums[r]
-            count[nums[r]] += 1
-            if r - l + 1 > k:
-                count[nums[l]] -= 1
-                if count[nums[l]] == 0:
-                    count.pop(nums[l])
-                currSum -= nums[l]
-                l += 1
-            if len(count) == k and r - l + 1 == k:
-                maxSub = max(currSum,maxSub)
-        return maxSub
+        max_Sum = 0
+        left = 0
+        curr_Sum = 0
+        freq = {}
+        for right in range(len(nums)):
+            curr_Sum += nums[right]
+            freq[nums[right]] = freq.get(nums[right],0)+1
+            if right-left+1 == k:
+                if len(freq) == k:
+                    max_Sum = max(max_Sum,curr_Sum)
+                curr_Sum -= nums[left]
+                freq[nums[left]] -= 1
+                if freq[nums[left]] == 0:
+                    del freq[nums[left]]
+                left += 1
+        return max_Sum
+
             
         
