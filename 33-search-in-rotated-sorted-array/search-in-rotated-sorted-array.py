7097820b9efb2209aceb14419f5295027df13fe7
preethibilla <42039081+preethibilla@users.noreply.github.com>
2025-06-21 18:27:13 -0700

    Time: 0 ms (100.00%) | Memory: 18.1 MB (66.63%) - LeetSync

--- 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.py
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.py
@@ -10,17 +10,21 @@ class Solution:
 
             # Left half is sorted
             if nums[left] <= nums[mid]:
-                if nums[left] <= target < nums[mid]:
-                    right = mid - 1
-                else:
+                if target > nums[mid] or target < nums[left]:
                     left = mid + 1
+                else:
+                    right = mid - 1
             # Right half is sorted
             else:
-                if nums[mid] < target <= nums[right]:
-                    left = mid + 1
-                else:
+                if target < nums[mid] or target > nums[right]:
                     right = mid - 1
-
+                else:
+                    left = mid + 1
         return -1
+       
+
+                    
+
+        
 
         
