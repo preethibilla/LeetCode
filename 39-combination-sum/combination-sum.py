95fdeefdfc752d5cd840b437a9e4c71e790139ad
preethibilla <42039081+preethibilla@users.noreply.github.com>
2025-06-22 10:24:34 -0700

    Time: 3 ms (97.52%) | Memory: 18 MB (39.50%) - LeetSync

--- 39-combination-sum/combination-sum.py
+++ 39-combination-sum/combination-sum.py
@@ -1,21 +1,20 @@
 class Solution:
     def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
-        res = []
-        candidates.sort()
+        candidates.sort()  # Optimization #1: sort to break early
+        result = []
 
-        def dfs(i, cur, total):
-            if total == target:
-                res.append(cur.copy())
+        def backtrack(start: int, path: List[int], target_left: int):
+            if target_left == 0:
+                result.append(path[:])
                 return
-            
-            for j in range(i, len(candidates)):
-                if total + candidates[j] > target:
-                    return
-                cur.append(candidates[j])
-                dfs(j, cur, total + candidates[j])
-                cur.pop()
-        
-        dfs(0, [], 0)
-        return res
+            for i in range(start, len(candidates)):
+                if candidates[i] > target_left:
+                    break  # No point in exploring further
+                path.append(candidates[i])
+                backtrack(i, path, target_left - candidates[i])  # Reuse same i
+                path.pop()
+
+        backtrack(0, [], target)
+        return result
 
         
