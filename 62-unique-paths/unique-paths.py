3cb6f00099a6d5c6a33164cc7eef6ccea58c4448
preethibilla <42039081+preethibilla@users.noreply.github.com>
2025-06-22 11:29:16 -0700

    Time: 0 ms (100.00%) | Memory: 17.8 MB (65.01%) - LeetSync

--- 62-unique-paths/unique-paths.py
+++ 62-unique-paths/unique-paths.py
@@ -1,19 +1,16 @@
 class Solution:
-    def __init__(self):
-        self.res = dict()
-
     def uniquePaths(self, m: int, n: int) -> int:
-        # Check for out of bound matrix cell
-        if m <= 0 or n <= 0:
-            return 0
-        # There is only one path to reach origin cell of matrix
-        if m == 1 and n == 1:
-            return 1
-        
-        # p(m, n) = Number of unique paths to reach cell (m, n) 
-        # p(m, n) = p(m-1, n) + p(m, n-1)   Only two ways to move right and down.
-        if (m, n) not in self.res:
-            self.res[(m, n)] = self.uniquePaths(m-1, n) + self.uniquePaths(m, n-1)
-        return self.res[(m, n)]
+        # Initialize a 1D DP array with all 1s
+        # There's only 1 way to reach any cell in the first row
+        dp = [1] * n
+
+        # Start filling the array row by row (from row 1 to m-1)
+        for i in range(1, m):
+            for j in range(1, n):
+                # dp[j] holds paths from above, dp[j - 1] holds paths from left
+                dp[j] += dp[j - 1]
+
+        # The last element contains the result for bottom-right cell
+        return dp[-1]
 
         
