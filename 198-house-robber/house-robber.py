5a91a3b7318c7b3dca08f836e363e97f363a2615
preethibilla <42039081+preethibilla@users.noreply.github.com>
2025-06-22 10:43:35 -0700

    Time: 0 ms (100.00%) | Memory: 17.6 MB (96.90%) - LeetSync

--- 198-house-robber/house-robber.py
+++ 198-house-robber/house-robber.py
@@ -1,20 +1,23 @@
 class Solution:
     def rob(self, nums: List[int]) -> int:
-        if not nums:
-            return 0
-        n = len(nums)
-        if n == 1:
-            return nums[0]
+        # rob1 stores the max money robbed up to the house before the previous (i-2)
+        # rob2 stores the max money robbed up to the previous house (i-1)
+        rob1, rob2 = 0, 0
 
-        # dp[i] = max money that can be robbed from house i to end
-        dp = [0] * n
-        dp[-1] = nums[-1]                  # Base case: only one house
-        dp[-2] = max(nums[-1], nums[-2])   # Base: choose better of last two
+        # Loop through each house (each value in nums)
+        for num in nums:
+            # temp stores the max money if we rob this house or skip it
+            # If we rob this house: num + rob1 (can't rob previous)
+            # If we skip it: rob2 (take the max up to previous)
+            temp = max(num + rob1, rob2)
 
-        for i in range(n - 3, -1, -1):
-            # Either rob current + dp[i+2] or skip to dp[i+1]
-            dp[i] = max(nums[i] + dp[i + 2], dp[i + 1])
+            # Move the rob1 and rob2 pointers forward:
+            # rob1 becomes previous rob2
+            # rob2 becomes the best value at this step (temp)
+            rob1 = rob2
+            rob2 = temp
 
-        return dp[0]
+        # After the loop, rob2 contains the max amount that can be robbed
+        return rob2
 
         
